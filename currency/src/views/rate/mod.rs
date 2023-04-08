e08e63b2a22eed1c9302a1cf77ab08efbf6236ad
Adam Pisula <adam.pisula@outlook.com>
2023-04-08 23:49:02 +0200

    update rate route

--- currency/src/views/rate/mod.rs
+++ currency/src/views/rate/mod.rs
@@ -2,9 +2,10 @@ pub mod delete;
 pub mod rate;
 pub mod remote;
 pub mod set;
+pub mod update;
 
 pub use delete::delete_route;
 pub use rate::rate_route;
 pub use remote::remote_route;
 pub use set::set_route;
-
+pub use update::update_route;
